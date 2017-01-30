f62910a39a5def16a4359a64e39428b520bfc6bc
Winter98Boy <miladansari789@gmail.com>
2017-01-30 11:47:54 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -230,7 +230,7 @@ function create_config( )
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
-An advance Administration bot based on yagop/telegram-bot 
+An advance Administration bot based on yagop/winter_shield 
 
 https://github.com/SEEDTEAM/TeleSeed
 
